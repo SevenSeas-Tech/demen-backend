2cd3d26908f7b062dc0baded566123cbf708ea4a
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-07 20:04:26 -0300

    feat(accounts/users): implementado o service de criaçao do usuario
    o service foi implementado parcialmente, a senha ainda esta em plain text.

--- src/modules/accounts/use-cases/users/create-user/CreateUser.service.ts
+++ src/modules/accounts/use-cases/users/create-user/CreateUser.service.ts
@@ -1,3 +1,37 @@
-class CreateUser {}
+import { inject, injectable } from 'tsyringe';
+
+import { CreateUserDto } from '@accounts:dtos/CreateUserDto';
+import { UserResponseDto } from '@accounts:dtos/UserResponseDto';
+import IUsersRepository from '@accounts:irepos/IUsersRepository';
+import UserMap from '@accounts:mapper/UserMap';
+import AppError from '@shared/errors/AppError';
+
+@injectable()
+class CreateUser {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute(data: CreateUserDto): Promise<UserResponseDto> {
+    const { username, email, name, lastName, password } = data;
+
+    const findByUsername = await this.usersRepository.findByUsername(username);
+
+    if (findByUsername) {
+      throw new AppError('Username already in use!');
+    }
+
+    const findByEmail = await this.usersRepository.findByEmail(email);
+
+    if (findByEmail) {
+      throw new AppError('Email already in use!');
+    }
+
+    const user = await this.usersRepository.create({ username, email, name, lastName, password });
+
+    return UserMap.toDto(user);
+  }
+}
 
 export default CreateUser;
