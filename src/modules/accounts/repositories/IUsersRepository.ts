370b4147924c1146ddf68457535094d9e56c173a
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-07 19:36:43 -0300

    feat(accounts/users): adicionados 2 metodos a interface do repositorio
    Adicionados os metodos para buscar por email e por username a interface do repositorio de usuarios.

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,8 +1,11 @@
 import { CreateUserDto } from '@accounts:dtos/CreateUserDto';
 import User from '@accounts:entities/User';
+import { Email } from '@accounts:types/User';
 
 interface IUsersRepository {
   create(data: CreateUserDto): Promise<User>;
+  findByEmail(email: Email): Promise<User | undefined>;
+  findByUsername(username: string): Promise<User | undefined>;
 }
 
 export default IUsersRepository;
