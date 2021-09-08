f878be0a83d5e2ba47b886a1daea1b3e607ca12c
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-07 21:07:13 -0300

    refactor(shared): alterado app -> App
    App e uma classe e deve ser escrito em PascalCase.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,9 +4,9 @@ import 'dotenv/config';
 import '@shared/infra/typeorm';
 import '@shared:containers/index';
 
-import app from './App';
+import App from './App';
 
-app.listen(process.env.PORT, () => {
+App.listen(process.env.PORT, () => {
   if (process.env.ENVIRONMENT === 'development') {
     console.log(`Listening at port: ${process.env.PORT}\n\
     Environment: ${process.env.ENVIRONMENT}
