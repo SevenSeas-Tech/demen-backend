73fce0b9acda81d7f06cd0043f814c6e07859b31
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-08 00:28:23 -0300

    feat(accounts/users): implementaçao da rota de criaçao de usuarios

--- src/modules/accounts/infra/http/routes/users.routes.ts
+++ src/modules/accounts/infra/http/routes/users.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import CreateUserController from '@accounts:use-cases/users/create-user/CreateUser.controller';
+
+const createUserController = new CreateUserController();
+
 const usersRouter = Router();
+
+usersRouter.post('/', createUserController.execute);
 
 export default usersRouter;
