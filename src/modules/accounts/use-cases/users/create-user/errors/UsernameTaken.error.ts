0f7823cb950daba8d82b64ca5dbacd1e918dc717
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-08 15:48:34 -0300

    refactor(accounts/users): rename das classes de erro

--- src/modules/accounts/use-cases/users/create-user/errors/UsernameTaken.error.ts
+++ src/modules/accounts/use-cases/users/create-user/errors/UsernameTaken.error.ts
@@ -1,9 +1,9 @@
 import AppError from '@shared/errors/AppError';
 
-class UsernameTaken extends AppError {
+class UsernameTakenError extends AppError {
   constructor() {
     super('Username is already taken!', 400);
   }
 }
 
-export default UsernameTaken;
+export default UsernameTakenError;
