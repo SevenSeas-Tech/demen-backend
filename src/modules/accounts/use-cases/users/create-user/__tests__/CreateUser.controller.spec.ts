01ea333e6a32cb41e7cc05de0bc7988d05888faf
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-08 16:49:39 -0300

    test(accounts/users): teste de integraçao de criaçao de usuario
    Implementado o teste de integraçao.
    
    Ele valida a criaçao do usuario.
    
    Ele valida as regras de username e email unicos.

--- src/modules/accounts/use-cases/users/create-user/__tests__/CreateUser.controller.spec.ts
+++ src/modules/accounts/use-cases/users/create-user/__tests__/CreateUser.controller.spec.ts
@@ -1,15 +1,95 @@
 import request from 'supertest';
+import { Connection } from 'typeorm';
 
+import createConnection from '@shared:typeorm/index';
 import App from '@shared/infra/http/App';
 
-// TODO: Implement tests
+let connection: Connection;
 
 describe('Create User Controller', () => {
-  beforeAll(() => {
-    request(App);
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
   });
 
   it('Should create a user', async () => {
-    expect(true);
+    const response = await request(App).post('/accounts/users').send({
+      username: 'foobar',
+      name: 'Foo',
+      lastName: 'Bar',
+      email: 'foobar@example.com',
+      password: 'secret',
+    });
+
+    const user = response.body;
+
+    expect(response.status).toEqual(201);
+    expect(user).toHaveProperty('username');
+    expect(user.username).toEqual('foobar');
+    expect(user).toHaveProperty('name');
+    expect(user.name).toEqual('Foo');
+    expect(user).toHaveProperty('lastName');
+    expect(user.lastName).toEqual('Bar');
+    expect(user).toHaveProperty('email');
+    expect(user.email).toEqual('foobar@example.com');
+    expect(user).toHaveProperty('createdAt');
+    expect(user).toHaveProperty('updatedAt');
+  });
+
+  it('should not create user if username is taken', async () => {
+    await request(App).post('/accounts/users').send({
+      username: 'foobar',
+      name: 'Foo',
+      lastName: 'Bar',
+      email: 'foobar@example.com',
+      password: 'secret',
+    });
+
+    const response = await request(App).post('/accounts/users').send({
+      username: 'foobar',
+      name: 'Foo',
+      lastName: 'Bar',
+      email: 'other@example.com',
+      password: 'secret',
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Username is already taken!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
+  });
+
+  it('should not create user if email is in use', async () => {
+    await request(App).post('/accounts/users').send({
+      username: 'foobar',
+      name: 'Foo',
+      lastName: 'Bar',
+      email: 'foobar@example.com',
+      password: 'secret',
+    });
+
+    const response = await request(App).post('/accounts/users').send({
+      username: 'other',
+      name: 'Foo',
+      lastName: 'Bar',
+      email: 'foobar@example.com',
+      password: 'secret',
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Email is already in use!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
   });
 });
