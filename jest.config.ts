b1e7893287ff661e4a0fabaec454f7317f364c97
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-08 16:39:02 -0300

    chore(jest): mudado o provider novamente para v8
    Eu entendi agora porque o v8 parece inconsistente.
    
    Ele gera o coverage do do service avaliando tambem o teste de controller.
    
    Babel:
    
    Quando todos os testes sao executados juntos os branchs sao 100%
    
    Isso ocorre porque ele gera o cover do service so pelo teste do service.
    
    V8:
    
    100% nos testes individuais, mas branchs faltando ao rodar todos.
    
    Parece um bug mas ele avalia o coverage do service tambem pelo teste de
    integraçao
    
    Se o teste de integraçao esta completo e for executado sozinho ele da
    100 de branch.
    
    Se o unitario estiver 100% e o de integraçao nao e eles forem executados
    juntos, os branch vao aparecer como nao cobertos.

--- jest.config.ts
+++ jest.config.ts
@@ -35,7 +35,7 @@ export default {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'babel',
+  coverageProvider: 'v8', // babel
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['text-summary', 'lcov'],
