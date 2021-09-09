8cd65056a39f153fccacf00f1ded2274bc5f606c
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-09 00:25:59 -0300

    refactor(accounts/sessions): foi removido username do pick de sessions

--- src/modules/accounts/@types/sessions/Sessions.d.ts
+++ src/modules/accounts/@types/sessions/Sessions.d.ts
@@ -2,7 +2,7 @@ import { UserResponseDto } from '@accounts:dtos/users/UserResponse.dto';
 import { Token } from '@accounts:types/tokens/Token';
 import { Credentials } from '@accounts:types/users/User';
 
-export type LoginCredentials = Pick<Credentials, 'username' | 'email' | 'password'>;
+export type LoginCredentials = Pick<Credentials, 'email' | 'password'>;
 
 export type SessionResponse = {
   user: UserResponseDto;
