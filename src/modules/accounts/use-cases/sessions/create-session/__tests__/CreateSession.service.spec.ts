5b3c158e9d2178a328014bcd37bb06d17bcb8176
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-10 22:00:50 -0300

    test(accounts/sessions): feita as implementaçoes necessarias apos a adiçao do provider
    Foram feitas as alteraçoes necessarias na criaçao do service.
    
    Foi criado um teste para saber se o validation provider foi chamado.
    
    Foi criado um teste esquecido para saber se o hash provider foi chamado.

--- src/modules/accounts/use-cases/sessions/create-session/__tests__/CreateSession.service.spec.ts
+++ src/modules/accounts/use-cases/sessions/create-session/__tests__/CreateSession.service.spec.ts
@@ -6,27 +6,39 @@ import FakeUsersRepository from '@accounts:irepos/fake/FakeUsers.repository';
 import IUsersRepository from '@accounts:irepos/IUsers.repository';
 import IHashProvider from '@shared/containers/providers/hash-provider/IHash.provider';
 import FakeHashProvider from '@shared/containers/providers/hash-provider/implementations/FakeHash.provider';
+import FakeValidationProvider from '@shared/containers/providers/validation-provider/FakeValidation.provider';
+import IValidationProvider from '@shared/containers/providers/validation-provider/IValidation.provider';
 
 import CreateSession from '../CreateSession.service';
 import InvalidCredentialsError from '../errors/InvalidCredentials.error';
+
+// ---------------------------------------------------------------------------------------------- //
 
 describe('Create session', () => {
   let usersRepository: IUsersRepository;
   let tokenProvider: ITokenProvider;
   let hashProvider: IHashProvider;
+  let validationProvider: IValidationProvider;
   let createSession: CreateSession;
 
   const username = 'foobar';
   const name = 'Foo';
   const lastName = 'Bar';
   const email = 'foobar@example.com';
-  const password = 'secret';
+  const password = 'Password12';
 
   beforeEach(async () => {
     usersRepository = new FakeUsersRepository();
     tokenProvider = new FakeTokenProvider();
     hashProvider = new FakeHashProvider();
-    createSession = new CreateSession(usersRepository, tokenProvider, hashProvider);
+    validationProvider = new FakeValidationProvider();
+
+    createSession = new CreateSession(
+      usersRepository,
+      tokenProvider,
+      hashProvider,
+      validationProvider,
+    );
 
     await usersRepository.create({
       username,
@@ -38,10 +50,16 @@ describe('Create session', () => {
   });
 
   it('should create a session', async () => {
+    const validateCredentials = jest.spyOn(validationProvider, 'validateLogin');
+    const match = jest.spyOn(hashProvider, 'match');
+
     const session = await createSession.execute({ email, password });
 
     const { user, token } = session;
     const isUuid = validate(session.user.id);
+
+    expect(validateCredentials).toHaveBeenCalled();
+    expect(match).toHaveBeenCalled();
 
     expect(session).toHaveProperty('user');
     expect(session).toHaveProperty('token');
