9cb092b26f4e69bee421fa5864a5d4e3beb14e1c
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-13 23:43:19 -0300

    test(accounts/users): implementados testes de validaçao do username e +
    Foram implementados testes novos para validar o username.
    
    Foram corrigidos e renomeados testes antigos para refletir as mudanças
    no codigo.

--- src/modules/accounts/use-cases/users/create-user/__tests__/CreateUser.controller.spec.ts
+++ src/modules/accounts/use-cases/users/create-user/__tests__/CreateUser.controller.spec.ts
@@ -7,9 +7,9 @@ import App from '@shared/infra/http/App';
 let connection: Connection;
 
 describe('Create User Controller', () => {
-  const name = 'Foo';
+  const name = 'foo';
   const email = 'foo@bar.com';
-  const lastName = 'Bar';
+  const lastName = 'bar';
   const password = 'Password12';
   const username = 'foobar';
 
@@ -24,7 +24,6 @@ describe('Create User Controller', () => {
   });
 
   // *** --------------------------------- User Creation ------------------------------------ *** //
-
   it('Should create a user', async () => {
     const response = await request(App).post('/accounts/users').send({
       username,
@@ -58,7 +57,6 @@ describe('Create User Controller', () => {
   });
 
   // *** ---------------------- Uniqueness Validation Tests --------------------------------- *** //
-
   it('should not create user if username is taken', async () => {
     const response = await request(App)
       .post('/accounts/users')
@@ -99,8 +97,7 @@ describe('Create User Controller', () => {
     expect(body.status).toEqual('error');
   });
 
-  // *** ------------------------- String Validation Tests ---------------------------------- *** //
-
+  // *** ------------------------- Email Validation Tests ---------------------------------- *** //
   it('should not create user with invalid email', async () => {
     const response = await request(App).post('/accounts/users').send({
       username,
@@ -119,6 +116,116 @@ describe('Create User Controller', () => {
     expect(body.status).toEqual('error');
   });
 
+  it('should not create user when email has spaces', async () => {
+    const response = await request(App).post('/accounts/users').send({
+      username,
+      name,
+      lastName,
+      email: ' foobar@example.com ',
+      password
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Invalid data!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
+  });
+
+  // *** ------------------------- User Validation Tests ------------------------------------ *** //
+  it('should not create user with username length < 5', async () => {
+    const response = await request(App).post('/accounts/users').send({
+      username: 'abcd',
+      name,
+      lastName,
+      email,
+      password
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Invalid data!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
+  });
+
+  it('should not create user with username is bigger than 16 characters', async () => {
+    const response = await request(App).post('/accounts/users').send({
+      username: 'foo_bar_huge_0017',
+      name,
+      lastName,
+      email,
+      password
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Invalid data!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
+  });
+
+  it('should not create user with username containing symbols', async () => {
+    const response = await request(App).post('/accounts/users').send({
+      username: 'foobar_?',
+      name,
+      lastName,
+      email,
+      password
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Invalid data!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
+  });
+
+  it('should not create user with username starting with number', async () => {
+    const response = await request(App).post('/accounts/users').send({
+      username: '3foobar',
+      name,
+      lastName,
+      email,
+      password
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Invalid data!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
+  });
+
+  it('should not create user when username has more than one word', async () => {
+    const response = await request(App).post('/accounts/users').send({
+      username: 'foobar bar',
+      name,
+      lastName,
+      email,
+      password
+    });
+
+    const { body } = response;
+
+    expect(response.status).toEqual(400);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toEqual('Invalid data!');
+    expect(body).toHaveProperty('status');
+    expect(body.status).toEqual('error');
+  });
+
+  // *** ------------------------- Name Validation Tests ------------------------------------ *** //
   it('should not create user with name length < 3', async () => {
     const response = await request(App).post('/accounts/users').send({
       username,
@@ -155,33 +262,31 @@ describe('Create User Controller', () => {
     expect(body.status).toEqual('error');
   });
 
-  it('should not create user with username length < 3', async () => {
-    const response = await request(App)
-      .post('/accounts/users')
-      .send({
-        username: 'ab',
-        name,
-        lastName,
-        email: `s3${email}`,
-        password
-      });
+  it('should create user without spaces on names', async () => {
+    const email = 'nospaces@names.com';
 
-    const { body } = response;
+    const response = await request(App).post('/accounts/users').send({
+      username: 'foobar_space',
+      name: ' foo ',
+      lastName: ' bar ',
+      email,
+      password
+    });
 
-    expect(response.status).toEqual(400);
-    expect(body).toHaveProperty('message');
-    expect(body.message).toEqual('Invalid data!');
-    expect(body).toHaveProperty('status');
-    expect(body.status).toEqual('error');
+    const user = response.body;
+
+    expect(response.status).toEqual(201);
+    expect(user.name).toEqual(name);
+    expect(user.lastName).toEqual(lastName);
   });
 
-  it('should not create user with spaces on names', async () => {
-    const username = 'foobar-names';
-    const email = 'nospaces@names.com';
+  it('should create user using lower letters on names', async () => {
+    const username = 'foobar_cap';
+    const email = 'nocapitalized@names.com';
     const response = await request(App).post('/accounts/users').send({
       username,
-      name: ' Foo ',
-      lastName: ' Bar ',
+      name: 'Foo',
+      lastName: 'Bar',
       email,
       password
     });
@@ -190,35 +295,8 @@ describe('Create User Controller', () => {
 
     expect(response.status).toEqual(201);
 
-    expect(user).toHaveProperty('username');
-    expect(user.username).toEqual(username);
-
-    expect(user).toHaveProperty('name');
     expect(user.name).toEqual(name);
-
-    expect(user).toHaveProperty('lastName');
     expect(user.lastName).toEqual(lastName);
-
-    expect(user).toHaveProperty('email');
-    expect(user.email).toEqual(email);
-  });
-
-  it('should not create user with spaces on email', async () => {
-    const response = await request(App).post('/accounts/users').send({
-      username,
-      name,
-      lastName,
-      email: ' foobar@example.com ',
-      password
-    });
-
-    const { body } = response;
-
-    expect(response.status).toEqual(400);
-    expect(body).toHaveProperty('message');
-    expect(body.message).toEqual('Invalid data!');
-    expect(body).toHaveProperty('status');
-    expect(body.status).toEqual('error');
   });
 
   // *** ------------------------ Password Validation --------------------------------------- *** //
