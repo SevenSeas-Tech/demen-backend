ba2571460d94542a407c6bf1303d6e96f47a1330
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-27 23:59:37 -0300

    chore(global/typeorm): mudado o parametro database para pegar a variavel de ambiente
    Por medida de segurança o ormconfig deve usar so variaveis de ambiente.
    
    Porque assim nao existe possibilidade dos dados secretos irem para o
    repositorio.

--- ormconfig.js
+++ ormconfig.js
@@ -3,9 +3,9 @@
   "type": "postgres",
   "host": process.env.DATABASE_URL,
   "port": process.env.DB_PORT,
-  "username": process.env.DB_USERNAME,
+  "username": process.env.DB_USER,
   "password": process.env.DB_PASSWORD,
-  "database": "demen",
+  "database": process.env.DB_DATABASE,
   "entities": [process.env.TYPEORM_ENTITIES],
   "migrations": [process.env.TYPEORM_MIGRATIONS],
   "cli": {
