a6730ca6dd59e97961d30358d573f99343d3e32d
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-28 23:28:33 -0300

    feat(admin/users): implementado o service que lista usuarios

--- src/modules/admin/use-cases/users/list-users/ListUsers.service.ts
+++ src/modules/admin/use-cases/users/list-users/ListUsers.service.ts
@@ -1,3 +1,29 @@
-class ListUsers {}
+import { inject, injectable } from 'tsyringe';
+
+import { UserResponseDto } from '@accounts:dtos/users/UserResponse.dto';
+import IUsersRepository from '@accounts:irepos/IUsers.repository';
+import UserMap from '@accounts:mapper/User.map';
+
+// ---------------------------------------------------------------------------------------------- //
+
+@injectable()
+class ListUsers {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(): Promise<UserResponseDto[]> {
+    const users = await this.usersRepository.findAll();
+
+    const mappedUsers = users.map(user => {
+      return UserMap.toDto(user);
+    });
+
+    return mappedUsers;
+  }
+}
+
+// ---------------------------------------------------------------------------------------------- //
 
 export default ListUsers;
