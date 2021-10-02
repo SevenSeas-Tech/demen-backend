3fc56ee46e2236d2a0af070f6bd0572f64dd5fa3
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-10-02 18:26:49 -0300

    refactor(shared/containers): renomeado o nome da classe do bcrypt

--- src/shared/containers/providers/index.ts
+++ src/shared/containers/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
 
 import IHashProvider from './hash-provider/IHash.provider';
-import Bcrypt from './hash-provider/implementations/Bcrypt.provider';
+import BcryptProvider from './hash-provider/implementations/Bcrypt.provider';
 import Yup from './validation-provider/implementations/Yup.provider';
 import IValidationProvider from './validation-provider/IValidation.provider';
 
-container.registerSingleton<IHashProvider>('HashProvider', Bcrypt);
+// ---------------------------------------------------------------------------------------------- //
+
+container.registerSingleton<IHashProvider>('HashProvider', BcryptProvider);
 
 container.registerSingleton<IValidationProvider>('ValidationProvider', Yup);
