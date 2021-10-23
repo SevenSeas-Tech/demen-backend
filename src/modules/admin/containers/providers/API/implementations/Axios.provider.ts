b123b6c640843e9930fe48dd1962e36c9ebfa754
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-10-23 20:59:04 -0300

    feat(admin/providers): foi adicionado um metodo ao axios para buscar os dados dos canais

--- src/modules/admin/containers/providers/API/implementations/Axios.provider.ts
+++ src/modules/admin/containers/providers/API/implementations/Axios.provider.ts
@@ -1,18 +1,45 @@
 import axios from 'axios';
 
-import IAPIProvider from '@admin:containers/providers/API/IAPI.provider';
+import IApiProvider from '@admin:containers/providers/API/IApi.provider';
+import { ApiChannelResponseDTO } from '@admin:dtos/g-api/ApiChannelResponse.dto';
 import { ApiVideoResponseDTO } from '@admin:dtos/g-api/ApiVideoResponse.dto';
+import { ChannelResponseDTO } from '@admin:dtos/videos/ChannelResponse.dto';
 import { VideoResponseDTO } from '@admin:dtos/videos/VideoResponse.dto';
 import GApiConfig from '@config/g-api/GApi.config';
 
 // ---------------------------------------------------------------------------------------------- //
 
-class AxiosProvider implements IAPIProvider {
+class AxiosProvider implements IApiProvider {
+  private apiKey = GApiConfig.apiKey;
+
+  // * ---------------------------------------------------------------------------------------- * //
+
+  async getChannel(id: string): Promise<ChannelResponseDTO> {
+    const { channelsUrl, channelFields: fields, parts } = GApiConfig;
+
+    const response = await axios.get(channelsUrl, {
+      params: { id, key: this.apiKey, part: parts, fields }
+    });
+
+    const { items } = response.data as ApiChannelResponseDTO;
+
+    const { description, title, thumbnails } = items.snippet;
+
+    return {
+      id,
+      description,
+      title,
+      thumbnail: thumbnails.url
+    };
+  }
+
+  // * ---------------------------------------------------------------------------------------- * //
+
   async getVideo(id: string): Promise<VideoResponseDTO> {
-    const { videos: videosUrl, apiKey, fields, parts } = GApiConfig;
+    const { videosUrl, videoFields: fields, parts } = GApiConfig;
 
     const response = await axios.get(videosUrl, {
-      params: { id, key: apiKey, part: parts, fields }
+      params: { id, key: this.apiKey, part: parts, fields }
     });
 
     const { items } = response.data as ApiVideoResponseDTO;
@@ -21,7 +48,7 @@ class AxiosProvider implements IAPIProvider {
 
     return {
       id,
-      channel: channelId,
+      channelId,
       description,
       title,
       thumbnail: thumbnails.default.url,
