e1958e5d49f215238e3cef44790ef2a0d9b49743
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-10-23 18:36:33 -0300

    refactor(admin/providers): foi refatorada a interface de api provider
    Foram feitas modificaçoes para refletir as alteraçoes nos dtos.

--- src/modules/admin/containers/providers/API/IAPI.provider.ts
+++ src/modules/admin/containers/providers/API/IAPI.provider.ts
@@ -1,5 +1,7 @@
+import { VideoResponseDTO } from '@admin:dtos/videos/VideoResponse.dto';
+
 interface IAPIProvider {
-  getVideo(id: string): Promise<void>;
+  getVideo(id: string): Promise<VideoResponseDTO>;
 }
 
 export default IAPIProvider;
