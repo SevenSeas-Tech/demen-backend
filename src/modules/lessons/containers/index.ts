82dc52bd5392c97a3faeaf88e8a00dd7fd5f1f6d
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-10-31 16:15:57 -0300

    feat(lessons): registrados os conatainers do modulo lessons

---
+++ src/modules/lessons/containers/index.ts
@@ -0,0 +1,16 @@
+import { container } from 'tsyringe';
+
+import IChannelsRepository from '@lessons:irepos/IChannels.repository';
+import ISubjectsRepository from '@lessons:irepos/ISubjects.repository';
+import IVideosRepository from '@lessons:irepos/IVideos.repository';
+import ChannelsRepository from '@lessons:repos/Channels.repository';
+import SubjectsRepository from '@lessons:repos/Subjects.repository';
+import VideosRepository from '@lessons:repos/Videos.repository';
+
+// ---------------------------------------------------------------------------------------------- //
+
+container.registerSingleton<IVideosRepository>('VideosRepository', VideosRepository);
+
+container.registerSingleton<IChannelsRepository>('ChannelsRepository', ChannelsRepository);
+
+container.registerSingleton<ISubjectsRepository>('SubjectsRepository', SubjectsRepository);
