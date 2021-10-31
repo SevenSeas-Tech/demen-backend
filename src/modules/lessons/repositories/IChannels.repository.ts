0d878eaf605a5598796c9aa1409b4a4f52f424ed
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-10-31 17:05:32 -0300

    feat(lessons/channel): adicionado o metodo find by  id a interface do repositorio de canal

--- src/modules/lessons/repositories/IChannels.repository.ts
+++ src/modules/lessons/repositories/IChannels.repository.ts
@@ -3,6 +3,7 @@ import Channel from '@lessons:entities/Channel';
 
 interface IChannelRepositories {
   create(data: CreateChannelDto): Promise<Channel>;
+  findById(id: string): Promise<Channel | undefined>;
 }
 
 export default IChannelRepositories;
