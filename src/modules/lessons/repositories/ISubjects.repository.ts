d396b056e05ee011677e757729298cc2d4bb9ca0
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-10-31 16:48:37 -0300

    feat(lessons/subject): adicionado o metodo find by id a interface do repositorio de subjects

--- src/modules/lessons/repositories/ISubjects.repository.ts
+++ src/modules/lessons/repositories/ISubjects.repository.ts
@@ -1,8 +1,10 @@
 import { CreateSubjectDto } from '@lessons:dtos/CreateSubject.dto';
 import Subject from '@lessons:entities/Subject';
+import { Uuid } from '@shared/@types/Uuid';
 
 interface ISubjectsRepository {
   create(data: CreateSubjectDto): Promise<Subject>;
+  findById(id: Uuid): Promise<Subject | undefined>;
 }
 
 export default ISubjectsRepository;
