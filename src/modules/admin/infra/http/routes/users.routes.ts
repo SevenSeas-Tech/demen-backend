0ad952961405e5c526373a375f120471dc0fa6e8
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-11-06 23:27:32 -0300

    refactor(admin/routes): alterada a exportaçao da roteador de usuarios

--- src/modules/admin/infra/http/routes/users.routes.ts
+++ src/modules/admin/infra/http/routes/users.routes.ts
@@ -4,6 +4,8 @@ import ListUsersController from '@admin:use-cases/users/list-users/ListUsers.con
 import AuthenticationMiddleware from '@shared/infra/http/middlewares/Authentication.middleware';
 import VerifyAdminStatusMiddleware from '@shared/infra/http/middlewares/VerifyAdminStatus.middleware';
 
+// ---------------------------------------------------------------------------------------------- //
+
 const authenticationMiddleware = new AuthenticationMiddleware();
 const verifyAdminStatusMiddleware = new VerifyAdminStatusMiddleware();
 
@@ -11,9 +13,13 @@ const usersRouter = Router();
 
 const listUsersController = new ListUsersController();
 
+// ---------------------------------------------------------------------------------------------- //
+
 usersRouter.use(authenticationMiddleware.execute);
 usersRouter.use(verifyAdminStatusMiddleware.execute);
 
 usersRouter.get('/', listUsersController.execute);
 
-export default usersRouter;
+// ---------------------------------------------------------------------------------------------- //
+
+export { usersRouter };
