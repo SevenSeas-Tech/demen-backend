eb6a1b265666d5482b785d1f112c524a52e9a036
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-11-23 15:34:28 -0300

    fix(migrations): corrigido a propriedade username que nao existe em staff member

--- src/shared/infra/typeorm/migrations/1637684783026-CreateStaff_members.ts
+++ src/shared/infra/typeorm/migrations/1637684783026-CreateStaff_members.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateStaffMembers1637684783026 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('create table staff_members (username integer) inherits (users)');
+    await queryRunner.query('create table staff_members () inherits (users)');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
