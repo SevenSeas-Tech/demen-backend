e6bd04e760062c8f8bf1664a0ae16d852a239084
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-11-23 15:38:43 -0300

    refactor(accounts/container): alterada a importaçao do irepositorio

--- src/modules/accounts/containers/index.ts
+++ src/modules/accounts/containers/index.ts
@@ -2,7 +2,7 @@ import '@accounts:containers/providers/';
 
 import { container } from 'tsyringe';
 
-import IUsersRepository from '@accounts:irepos/IUsers.repository';
+import { IUsersRepository } from '@accounts:irepos/IUsers.repository';
 import UsersRepository from '@accounts:repos/Users.repository';
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
