b02dc7da02f81870da691304b01690198b4d77ed
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-11-23 15:45:27 -0300

    refactor(accounts/repositories): mudada a exportaçao da interface de repositorio de usuarios

--- src/modules/accounts/repositories/IUsers.repository.ts
+++ src/modules/accounts/repositories/IUsers.repository.ts
@@ -1,9 +1,12 @@
 import { CreateUserDto } from '@accounts:dtos/users/CreateUser.dto';
 import { UpdateUserDto } from '@accounts:dtos/users/UpdateUser.dto';
-import User from '@accounts:entities/User';
-import { Email, Uuid } from '@accounts:types/users/User';
+import { User } from '@accounts:entities/User';
+import { Email } from '@accounts:types/users/User';
+import { Uuid } from '@shared/@types/Uuid';
 
-interface IUsersRepository {
+// ---------------------------------------------------------------------------------------------- //
+
+export interface IUsersRepository {
   create(data: CreateUserDto): Promise<User>;
   findAll(): Promise<User[]>;
   findById(id: Uuid): Promise<User | undefined>;
@@ -11,5 +14,3 @@ interface IUsersRepository {
   findByUsername(username: string): Promise<User | undefined>;
   update(data: UpdateUserDto): Promise<User>;
 }
-
-export default IUsersRepository;
