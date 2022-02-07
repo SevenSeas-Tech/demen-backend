7eeb4a9ec43c866b027328f6fc3b60414ddfa27a
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-06 21:27:23 -0300

    feat(accounts/users): alterado o controller devido ao login social

--- src/modules/accounts/use-cases/users/create-user/CreateUser.controller.ts
+++ src/modules/accounts/use-cases/users/create-user/CreateUser.controller.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-import CreateUser from './CreateUser.service';
+import { CreateUserService } from './CreateUser.service';
 
 class CreateUserController {
   async execute(request: Request, response: Response): Promise<Response> {
-    const { username, name, lastName, email, password } = request.body;
-    const createUser = container.resolve(CreateUser);
+    const { idToken } = request.body;
+    const createUser = container.resolve(CreateUserService);
 
-    const user = await createUser.execute({ username, name, lastName, email, password });
+    const user = await createUser.execute({ idToken });
 
     return response.status(201).json(user);
   }
 }
 
-export default CreateUserController;
+export { CreateUserController };
