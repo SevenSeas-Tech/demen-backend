d1e5515d66a2486a03f9c469698eedea1a397615
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-12 20:59:57 -0300

    feat(types): adicionada a propriedade employee a request do express

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -3,12 +3,22 @@ declare namespace Express {
   export interface Request {
     user: {
       id: string;
+      googleId: string;
+      avatar: string;
+      email: string;
+      name: string;
+      lastName: string;
+      createdAt: Date;
+      updatedAt: Date;
+    };
+
+    employee: {
+      id: string;
       username: string;
       email: string;
       name: string;
       lastName: string;
-      admin: boolean;
-      verified: boolean;
+      phone: string;
       videos?: Video[];
       createdAt: Date;
       updatedAt: Date;
