da57cbfa84046a2157d7148f271e84dee0e624e1
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-12 15:25:24 -0300

    refactor(shared/providers): corrigido imports e exports do provider de validaçao de dados

--- src/shared/containers/providers/validation-provider/IValidation.provider.ts
+++ src/shared/containers/providers/validation-provider/IValidation.provider.ts
@@ -1,6 +1,6 @@
-import { CreateEmployeeDto } from '@accounts:dtos/users/CreateEmployee.dto';
+import { CreateEmployeeDto } from '@accounts:dtos/employees/CreateEmployee.dto';
+import { UpdateEmployeeDto } from '@accounts:dtos/employees/UpdateEmployee.dto';
 import { CreateUserDto } from '@accounts:dtos/users/CreateUser.dto';
-import { UpdateEmployeeDto } from '@accounts:dtos/users/UpdateEmployee.dto';
 import { UpdateUserDto } from '@accounts:dtos/users/UpdateUser.dto';
 import { LoginCredentials } from '@accounts:types/sessions/Sessions';
 
--- src/shared/containers/providers/validation-provider/implementations/Yup.provider.ts
+++ src/shared/containers/providers/validation-provider/implementations/Yup.provider.ts
@@ -2,9 +2,9 @@ import { validate } from 'uuid';
 import * as yup from 'yup';
 import YupPassword from 'yup-password';
 
-import { CreateEmployeeDto } from '@accounts:dtos/users/CreateEmployee.dto';
+import { CreateEmployeeDto } from '@accounts:dtos/employees/CreateEmployee.dto';
+import { UpdateEmployeeDto } from '@accounts:dtos/employees/UpdateEmployee.dto';
 import { CreateUserDto } from '@accounts:dtos/users/CreateUser.dto';
-import { UpdateEmployeeDto } from '@accounts:dtos/users/UpdateEmployee.dto';
 import { UpdateUserDto } from '@accounts:dtos/users/UpdateUser.dto';
 import { LoginCredentials } from '@accounts:types/sessions/Sessions';
 
@@ -107,4 +107,4 @@ class Yup implements IValidationProvider {
   }
 }
 
-export default Yup;
+export { Yup };
