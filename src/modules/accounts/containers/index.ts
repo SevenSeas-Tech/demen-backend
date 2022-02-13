f70aa4cfd4da8c70f82655d3079089fbe7d0eb50
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-13 09:36:15 -0300

    refactor(accounts/tsyringe): alteraçao no import

--- src/modules/accounts/containers/index.ts
+++ src/modules/accounts/containers/index.ts
@@ -3,6 +3,6 @@ import '@accounts:containers/providers/';
 import { container } from 'tsyringe';
 
 import { IUsersRepository } from '@accounts:irepos/IUsers.repository';
-import UsersRepository from '@accounts:repos/Users.repository';
+import { UsersRepository } from '@accounts:repos/Users.repository';
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
