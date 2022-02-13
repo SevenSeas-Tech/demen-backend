f84d9701fe5ce4237bb3864266556cfb395cf226
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-13 18:15:36 -0300

    refactor(accounts/routes): removida a rota de sessao de usuario

--- src/modules/accounts/infra/http/routes/sessions.routes.ts
+++ src/modules/accounts/infra/http/routes/sessions.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
 
 import { CreateEmployeeSessionController } from '@accounts:use-cases/sessions/create-employee-session/CreateEmployeeSession.controller';
-import { CreateUserSessionController } from '@accounts:use-cases/sessions/create-user-session/CreateUserSession.controller';
 
 const sessionsRouter = Router();
 
 // *** -------------------- Controllers ----------------------------------------------------- *** //
 
-const createUserSessionController = new CreateUserSessionController();
 const createEmployeeSessionController = new CreateEmployeeSessionController();
 
 // *** -------------------- Routes ---------------------------------------------------------- *** //
 
-sessionsRouter.post('/user', createUserSessionController.execute);
+// sessionsRouter.post('/user', createUserSessionController.execute);
 sessionsRouter.post('/employees', createEmployeeSessionController.execute);
 
 // ---------------------------------------------------------------------------------------------- //
