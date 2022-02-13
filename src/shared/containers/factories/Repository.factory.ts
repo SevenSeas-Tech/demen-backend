adfa3ba4c70e637a085b17aadd0482064662396e
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-12 21:01:19 -0300

    feat(shared/factories): adicionado o repositorio de colaboradores a fabrica de repositorios

--- src/shared/containers/factories/Repository.factory.ts
+++ src/shared/containers/factories/Repository.factory.ts
@@ -1,11 +1,15 @@
+import { IEmployeesRepository } from '@accounts:irepos/IEmployees.repository';
 import { IUsersRepository } from '@accounts:irepos/IUsers.repository';
+import { EmployeesRepository } from '@accounts:repos/Employees.repository';
 import { UsersRepository } from '@accounts:repos/Users.repository';
 
 class RepositoryFactory {
   UsersRepository: IUsersRepository;
+  EmployeesRepository: IEmployeesRepository;
 
   constructor() {
     this.UsersRepository = new UsersRepository();
+    this.EmployeesRepository = new EmployeesRepository();
   }
 }
 
