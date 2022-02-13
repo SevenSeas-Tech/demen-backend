4f38618e2c77534f76f097e9f6e0fb62c63c3f31
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-13 17:01:24 -0300

    fix(accounts/routes): removidas rotas de usuarios desnecessarias

--- src/modules/accounts/infra/http/routes/users.routes.ts
+++ src/modules/accounts/infra/http/routes/users.routes.ts
@@ -3,33 +3,31 @@ import { Router } from 'express';
 import { CreateUserController } from '@accounts:use-cases/users/create-user/CreateUser.controller';
 import { ListUsersController } from '@accounts:use-cases/users/list-users/ListUsers.controller';
 import { ShowUserProfileController } from '@accounts:use-cases/users/show-user-profile/ShowUserProfile.controller';
-import { UpdateUserController } from '@accounts:use-cases/users/update-user/UpdateUser.controller';
 import { EmployeeAuthenticationMiddleware } from '@shared/infra/http/middlewares/EmployeeAuthentication.middleware';
-import { UserAuthenticationMiddleware } from '@shared/infra/http/middlewares/UserAuthentication.middleware';
+// import { UserAuthenticationMiddleware } from '@shared/infra/http/middlewares/UserAuthentication.middleware';
 
 // ---------------------------------------------------------------------------------------------- //
 
 const usersRouter = Router();
 
 // *** ---- Middlewares --------------------------------------------------------------------- *** //
-const userAuthenticationMiddleware = new UserAuthenticationMiddleware();
+// const userAuthenticationMiddleware = new UserAuthenticationMiddleware();
 const employeeAuthenticationMiddleware = new EmployeeAuthenticationMiddleware();
 
 // *** ---- Controllers --------------------------------------------------------------------- *** //
 const createUserController = new CreateUserController();
 const showProfileController = new ShowUserProfileController();
-const updateUserController = new UpdateUserController();
 const listUsersController = new ListUsersController();
 
 // *** ---- Client Routes ------------------------------------------------------------------- *** //
 usersRouter.post('/', createUserController.execute);
 
-usersRouter.get('/profile', userAuthenticationMiddleware.execute, showProfileController.execute);
-
-usersRouter.patch('/profile', userAuthenticationMiddleware.execute, updateUserController.execute);
+// usersRouter.get('/profile', userAuthenticationMiddleware.execute, showProfileController.execute);
 
 // *** ---- Admin routes -------------------------------------------------------------------- *** //
 usersRouter.use(employeeAuthenticationMiddleware.execute);
+
+usersRouter.get('/dashboard/profile', showProfileController.execute);
 
 usersRouter.get('/', listUsersController.execute);
 
