b097cd241bbee1f021b8419273afd0b40be1f83d
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-12 21:02:45 -0300

    refactor(shared/containers): modificaçoes de import e export

--- src/shared/containers/providers/index.ts
+++ src/shared/containers/providers/index.ts
@@ -1,9 +1,9 @@
 import { container } from 'tsyringe';
 
-import IHashProvider from './hash-provider/IHash.provider';
-import BcryptProvider from './hash-provider/implementations/Bcrypt.provider';
-import Yup from './validation-provider/implementations/Yup.provider';
-import IValidationProvider from './validation-provider/IValidation.provider';
+import { IHashProvider } from './hash-provider/IHash.provider';
+import { BcryptProvider } from './hash-provider/implementations/Bcrypt.provider';
+import { Yup } from './validation-provider/implementations/Yup.provider';
+import { IValidationProvider } from './validation-provider/IValidation.provider';
 
 // ---------------------------------------------------------------------------------------------- //
 
