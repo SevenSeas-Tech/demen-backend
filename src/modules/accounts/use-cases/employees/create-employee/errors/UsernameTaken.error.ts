e8d8a3a5f5c26574b5f5b581dc748ab74299ec7d
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-12 23:14:02 -0300

    refactor(accounts/employees): correçao de import e export

--- src/modules/accounts/use-cases/employees/create-employee/errors/UsernameTaken.error.ts
+++ src/modules/accounts/use-cases/employees/create-employee/errors/UsernameTaken.error.ts
@@ -1,9 +1,7 @@
-import AppError from '@shared/errors/App.error';
+import { AppError } from '@shared/errors/App.error';
 
-class UsernameTakenError extends AppError {
+export class UsernameTakenError extends AppError {
   constructor() {
     super('Username is already taken!', 400);
   }
 }
-
-export { UsernameTakenError };
