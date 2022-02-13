8202fb734ce92beda03ca60e222f0b05f9ce5379
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-13 19:29:49 -0300

    test(accounts/employees): testes de integraçao para perfil de colaborador

--- src/modules/accounts/use-cases/employees/show-employee-profile/__tests__/ShowEmployeeProfile.controller.spec.ts
+++ src/modules/accounts/use-cases/employees/show-employee-profile/__tests__/ShowEmployeeProfile.controller.spec.ts
@@ -1,19 +1,26 @@
 import request from 'supertest';
 import { Connection } from 'typeorm';
 
+import { Employee } from '@accounts:entities/Employee';
 import createConnection from '@shared:typeorm/index';
 import App from '@shared/infra/http/App';
+// eslint-disable-next-line import-helpers/order-imports
+import { ProviderFactory } from '@shared/containers/factories/Provider.factory';
 
-// ---------------------------------------------------------------------------------------------- //
+// ! ---- Factory must come after App ----------------------------------------------------------- //
 
-describe('Create Employee Controller', () => {
+describe('Update Employee Controller', () => {
   let connection: Connection;
+  const hashProvider = new ProviderFactory().HashProvider;
 
   const name = 'foo';
   const email = 'foo@bar.com';
   const lastName = 'bar';
   const password = 'Password12';
   const username = 'foobar';
+  const phone = '99 99999 9999';
+
+  let employee: Employee;
 
   // -------------------------------------------------------------------------------------------- //
 
@@ -21,13 +28,12 @@ describe('Create Employee Controller', () => {
     connection = await createConnection();
     await connection.runMigrations();
 
-    await request(App).post('/accounts/users').send({
-      username,
-      name,
-      lastName,
-      email,
-      password
-    });
+    const passwordHash = await hashProvider.hash(password);
+
+    await connection.query(`INSERT INTO employees (username, email, name, last_name, password, phone)
+    VALUES ('${username}', '${email}', '${name}', '${lastName}', '${passwordHash}', '${phone}')`);
+
+    employee = await connection.query(`SELECT * FROM employees WHERE username='${username}'`);
   });
 
   afterAll(async () => {
@@ -37,35 +43,33 @@ describe('Create Employee Controller', () => {
 
   // -------------------------------------------------------------------------------------------- //
 
-  it('should show users profile', async () => {
-    const session = await request(App).post('/accounts/sessions').send({
+  it('should show employee profile', async () => {
+    const session = await request(App).post('/accounts/sessions/employees').send({
       email,
       password
     });
 
-    const { user, token } = session.body;
+    const { employee, token } = session.body;
 
     const response = await request(App)
-      .get('/accounts/users/profile')
-      .set({ Authorization: `Bearer ${token}` });
+      .get(`/accounts/employees/${employee.id}`)
+      .set({ authorization: `Bearer ${token}` });
 
     const { body } = response;
 
     expect(response.status).toEqual(201);
-
     expect(body).toHaveProperty('id');
-    expect(body.id).toEqual(user.id);
+    expect(body.id).toEqual(employee.id);
 
     expect(body).not.toHaveProperty('password');
-    expect(body).not.toHaveProperty('admin');
 
-    expect(body).toEqual(user);
+    expect(body).toEqual(employee);
   });
 
   // -------------------------------------------------------------------------------------------- //
 
   it('should not return profile of unauthenticated user', async () => {
-    const response = await request(App).get('/accounts/users/profile');
+    const response = await request(App).get(`/accounts/employees/${employee.id}`);
 
     const { body } = response;
     expect(response.status).toEqual(401);
