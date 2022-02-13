2cba08937d944d15609bfa59d8344523feba20c9
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-13 15:29:04 -0300

    test(accounts/sessions): testes de integraçao na criaçao de sessao de colaboradores

--- src/modules/accounts/use-cases/sessions/create-employee-session/__tests__/CreateEmployeeSession.controller.spec.ts
+++ src/modules/accounts/use-cases/sessions/create-employee-session/__tests__/CreateEmployeeSession.controller.spec.ts
@@ -3,18 +3,22 @@ import { Connection } from 'typeorm';
 import { validate } from 'uuid';
 
 import createConnection from '@shared:typeorm/index';
+import { BcryptProvider } from '@shared/containers/providers/hash-provider/implementations/Bcrypt.provider';
 import App from '@shared/infra/http/App';
 
 // ---------------------------------------------------------------------------------------------- //
 
-describe('Create Session Controller', () => {
+describe('Create Employee Session Controller', () => {
   let connection: Connection;
+
+  const hashProvider = new BcryptProvider();
 
   const username = 'foobar';
   const name = 'foo';
   const lastName = 'bar';
   const email = 'foobar@example.com';
   const password = 'Password12';
+  const phone = '99 99999 9999';
 
   // -------------------------------------------------------------------------------------------- //
 
@@ -22,13 +26,10 @@ describe('Create Session Controller', () => {
     connection = await createConnection();
     await connection.runMigrations();
 
-    await request(App).post('/accounts/users').send({
-      username,
-      name,
-      lastName,
-      email,
-      password
-    });
+    const passwordHash = await hashProvider.hash(password);
+
+    await connection.query(`INSERT INTO employees (username, email, name, last_name, password, phone)
+    VALUES ('${username}', '${email}', '${name}', '${lastName}', '${passwordHash}', '${phone}')`);
   });
 
   afterAll(async () => {
@@ -39,40 +40,42 @@ describe('Create Session Controller', () => {
   // -------------------------------------------------------------------------------------------- //
 
   it('should create a session', async () => {
-    const response = await request(App).post('/accounts/sessions').send({ email, password });
+    const response = await request(App)
+      .post('/accounts/sessions/employees')
+      .send({ email, password });
 
-    const { user, token } = response.body;
+    const { employee, token } = response.body;
 
-    const isUuid = validate(user.id);
+    const isUuid = validate(employee.id);
 
     expect(response.status).toEqual(201);
 
-    expect(response.body).toHaveProperty('user');
+    expect(response.body).toHaveProperty('employee');
     expect(response.body).toHaveProperty('token');
     expect(token).toBeTruthy();
 
-    expect(user).toHaveProperty('id');
+    expect(employee).toHaveProperty('id');
     expect(isUuid).toBeTruthy();
 
-    expect(user).not.toHaveProperty('password');
-    expect(user).not.toHaveProperty('admin');
+    expect(employee).not.toHaveProperty('password');
+    expect(employee).not.toHaveProperty('admin');
 
-    expect(user).toHaveProperty('username');
-    expect(user.username).toEqual(username);
+    expect(employee).toHaveProperty('username');
+    expect(employee.username).toEqual(username);
 
-    expect(user).toHaveProperty('name');
-    expect(user.name).toEqual(name);
+    expect(employee).toHaveProperty('name');
+    expect(employee.name).toEqual(name);
 
-    expect(user).toHaveProperty('lastName');
-    expect(user.lastName).toEqual(lastName);
+    expect(employee).toHaveProperty('lastName');
+    expect(employee.lastName).toEqual(lastName);
 
-    expect(user).toHaveProperty('email');
-    expect(user.email).toEqual(email);
+    expect(employee).toHaveProperty('email');
+    expect(employee.email).toEqual(email);
 
-    expect(user).toHaveProperty('createdAt');
-    expect(user.createdAt).toBeTruthy();
-    expect(user).toHaveProperty('updatedAt');
-    expect(user.updatedAt).toBeTruthy();
+    expect(employee).toHaveProperty('createdAt');
+    expect(employee.createdAt).toBeTruthy();
+    expect(employee).toHaveProperty('updatedAt');
+    expect(employee.updatedAt).toBeTruthy();
   });
 
   // -------------------------------------------------------------------------------------------- //
@@ -80,7 +83,9 @@ describe('Create Session Controller', () => {
   it('should not create session with invalid email', async () => {
     const email = 'foobar2@example.com';
 
-    const response = await request(App).post('/accounts/sessions').send({ email, password });
+    const response = await request(App)
+      .post('/accounts/sessions/employees')
+      .send({ email, password });
 
     const { body } = response;
 
@@ -96,7 +101,9 @@ describe('Create Session Controller', () => {
   it('should not create session with invalid password', async () => {
     const password = 'Password14';
 
-    const response = await request(App).post('/accounts/sessions').send({ email, password });
+    const response = await request(App)
+      .post('/accounts/sessions/employees')
+      .send({ email, password });
 
     const { body } = response;
 
@@ -112,7 +119,9 @@ describe('Create Session Controller', () => {
   it('should not create session if password validation failed', async () => {
     const password = 'password12';
 
-    const response = await request(App).post('/accounts/sessions').send({ email, password });
+    const response = await request(App)
+      .post('/accounts/sessions/employees')
+      .send({ email, password });
     const { body } = response;
 
     expect(response.status).toEqual(400);
@@ -127,7 +136,9 @@ describe('Create Session Controller', () => {
   it('should not create session if email validation failed', async () => {
     const email = 'invalid-email';
 
-    const response = await request(App).post('/accounts/sessions').send({ email, password });
+    const response = await request(App)
+      .post('/accounts/sessions/employees')
+      .send({ email, password });
 
     const { body } = response;
 
