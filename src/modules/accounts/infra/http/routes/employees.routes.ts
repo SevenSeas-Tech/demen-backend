d9222ed3219a045c18cfc65a8aef17df9af3b355
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-13 18:28:49 -0300

    feat(accounts/routes): criada rota para listagem de colaboradores

--- src/modules/accounts/infra/http/routes/employees.routes.ts
+++ src/modules/accounts/infra/http/routes/employees.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateEmployeeController } from '@accounts:use-cases/employees/create-employee/CreateEmployee.controller';
+import { ListEmployeesController } from '@accounts:use-cases/employees/list-employees/ListEmployees.controller';
 import { ShowEmployeeProfileController } from '@accounts:use-cases/employees/show-employee-profile/ShowEmployeeProfile.controller';
 import { UpdateEmployeeController } from '@accounts:use-cases/employees/update-employee/UpdateEmployee.controller';
 import { EmployeeAuthenticationMiddleware } from '@shared/infra/http/middlewares/EmployeeAuthentication.middleware';
@@ -14,6 +15,7 @@ const employeeAuthenticationMiddleware = new EmployeeAuthenticationMiddleware();
 const createEmployeeController = new CreateEmployeeController();
 const showEmployeeProfileController = new ShowEmployeeProfileController();
 const updateEmployeeController = new UpdateEmployeeController();
+const listEmployeesController = new ListEmployeesController();
 
 // ---------------------------------------------------------------------------------------------- //
 employeesRouter.use(employeeAuthenticationMiddleware.execute);
@@ -23,6 +25,8 @@ employeesRouter.get('/profile', showEmployeeProfileController.execute);
 employeesRouter.patch('/profile', updateEmployeeController.execute);
 
 employeesRouter.post('/', createEmployeeController.execute);
+
+employeesRouter.get('/', listEmployeesController.execute);
 
 // ---------------------------------------------------------------------------------------------- //
 
