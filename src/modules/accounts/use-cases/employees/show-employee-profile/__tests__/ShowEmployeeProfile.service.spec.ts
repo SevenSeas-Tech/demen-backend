e9852a6e4e8b897c2e037f4970b1300a625cc5f0
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-02-13 19:17:25 -0300

    test(accounts/employees): testes do serviço de perfil de colaborador

--- src/modules/accounts/use-cases/employees/show-employee-profile/__tests__/ShowEmployeeProfile.service.spec.ts
+++ src/modules/accounts/use-cases/employees/show-employee-profile/__tests__/ShowEmployeeProfile.service.spec.ts
@@ -1,30 +1,35 @@
+import { Employee } from '@accounts:entities/Employee';
+import { FakeEmployeesRepository } from '@accounts:irepos/fake/FakeEmployees.repository';
+import { IEmployeesRepository } from '@accounts:irepos/IEmployees.repository';
+import { NotFoundError } from '@shared/infra/http/middlewares/errors/NotFound.error';
+
 import { ShowEmployeeProfileService } from '../ShowEmployeeProfile.service';
 
 // ---------------------------------------------------------------------------------------------- //
 
 describe('Show Employee Profile Service', () => {
   let showEmployeeProfile: ShowEmployeeProfileService;
-
-  const employee = {
-    id: 'id',
-    name: 'foo',
-    lastName: 'bar',
-    email: 'foobar@example.com',
-    password: 'Password14',
-    username: 'foobar',
-    phone: '99 99999 9999',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  };
+  let employeesRepository: IEmployeesRepository;
+  let employee: Employee;
 
   beforeEach(async () => {
-    showEmployeeProfile = new ShowEmployeeProfileService();
+    employeesRepository = new FakeEmployeesRepository();
+    showEmployeeProfile = new ShowEmployeeProfileService(employeesRepository);
+
+    employee = await employeesRepository.create({
+      name: 'foo',
+      lastName: 'bar',
+      email: 'foobar@example.com',
+      password: 'Password14',
+      username: 'foobar',
+      phone: '99 99999 9999'
+    });
   });
 
   // -------------------------------------------------------------------------------------------- //
 
   it('should return employee profile', async () => {
-    const profile = await showEmployeeProfile.execute({ employee });
+    const profile = await showEmployeeProfile.execute({ id: employee.id });
 
     expect(profile).toHaveProperty('id');
     expect(profile.id).toEqual(employee.id);
@@ -33,7 +38,6 @@ describe('Show Employee Profile Service', () => {
     expect(profile.username).toEqual(employee.username);
 
     expect(profile).not.toHaveProperty('password');
-    expect(profile).not.toHaveProperty('admin');
 
     expect(profile).toHaveProperty('name');
     expect(profile.name).toEqual(employee.name);
@@ -41,10 +45,25 @@ describe('Show Employee Profile Service', () => {
     expect(profile).toHaveProperty('lastName');
     expect(profile.lastName).toEqual(employee.lastName);
 
+    expect(profile).toHaveProperty('phone');
+    expect(profile.phone).toEqual(employee.phone);
+
     expect(profile).toHaveProperty('email');
     expect(profile.email).toEqual(employee.email);
 
     expect(profile).toHaveProperty('createdAt');
     expect(profile).toHaveProperty('updatedAt');
+  });
+
+  it('should not return inexistent user profile', async () => {
+    expect(async () => {
+      await showEmployeeProfile.execute({ id: 'ffffaaaa-ffff-ffff-ffff-ffffaaaaffff' });
+    }).rejects.toEqual(new NotFoundError());
+  });
+
+  it('should not show profile of invalid id', async () => {
+    expect(async () => {
+      await showEmployeeProfile.execute({ id: 'invalid-id' });
+    }).rejects.toEqual(new NotFoundError());
   });
 });
