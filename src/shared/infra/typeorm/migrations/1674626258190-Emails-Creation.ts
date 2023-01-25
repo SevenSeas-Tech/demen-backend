cc33b1cda9e4e5fec8a226fd0eeca7090ba058ee
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-25 03:17:37 -0300

    feat(migrations): criadas mais duas migrations
    Criadas as migrations de emails e de managers

---
+++ src/shared/infra/typeorm/migrations/1674626258190-Emails-Creation.ts
@@ -0,0 +1,73 @@
+import { Table } from 'typeorm';
+
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+// * ------------------------------------------------------------------------------------------ * //
+
+export class EmailsCreation1674626258190 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const emails = new Table({
+      name: 'emails',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          default: 'uuid_generate_v4()'
+        },
+        {
+          name: 'user_id',
+          type: 'uuid'
+        },
+        {
+          name: 'type_id',
+          type: 'uuid'
+        },
+        {
+          name: 'verified',
+          type: 'boolean'
+        },
+        {
+          name: 'email',
+          type: 'varchar'
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()'
+        }
+      ],
+      foreignKeys: [
+        {
+          name: 'FK_email_user',
+          referencedTableName: 'users',
+          referencedColumnNames: ['id'],
+          columnNames: ['user_id'],
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE'
+        },
+        {
+          name: 'FK_email_type',
+          referencedTableName: 'email_types',
+          referencedColumnNames: ['id'],
+          columnNames: ['type_id'],
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE'
+        }
+      ]
+    });
+
+    await queryRunner.createTable(emails);
+  }
+
+  // -------------------------------------------------------------------------------------------- //
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('emails');
+  }
+}
---
+++ src/shared/infra/typeorm/migrations/1674626931865-Managers-Creation.ts
@@ -0,0 +1,15 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+// * ------------------------------------------------------------------------------------------ * //
+
+export class ManagersCreation1674626931865 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('create table managers (password varchar) inherits (users)');
+  }
+
+  // -------------------------------------------------------------------------------------------- //
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('managers');
+  }
+}
