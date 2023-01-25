13bd783697cd7c5ba6b3e0e47076e38754cb396b
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-25 02:24:09 -0300

    feat(migrations): criadas duas migrations
    Criadas as migrations de email_types e token_types.

---
+++ src/shared/infra/typeorm/migrations/1674623392303-Token_Types_Creation.ts
@@ -0,0 +1,41 @@
+import { Table } from 'typeorm';
+
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+// * ------------------------------------------------------------------------------------------ * //
+
+export class TokenTypesCreation1674623392303 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const tokenTypes = new Table({
+      name: 'token_types',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          default: 'uuid_generate_v4()'
+        },
+        {
+          name: 'type',
+          type: 'varchar'
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()'
+        }
+      ]
+    });
+
+    await queryRunner.createTable(tokenTypes);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('token_types');
+  }
+}
