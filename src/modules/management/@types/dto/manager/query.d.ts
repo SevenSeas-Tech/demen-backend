1d11e6522d3bd894eb30ac4b0e0dbf26ce3396d0
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-13 14:32:38 -0300

    feat(management): criados dois novos dto's
    Criado o dto de update de manager e um dto para filtrar os dados de relações do manager.

---
+++ src/modules/management/@types/dto/manager/query.d.ts
@@ -0,0 +1,7 @@
+export interface ManagerQueryOptions {
+  emails: boolean;
+  issues: boolean;
+  phones: boolean;
+  tokens: boolean;
+  videos: boolean;
+}
---
+++ src/modules/management/@types/dto/manager/update.d.ts
@@ -0,0 +1,10 @@
+import type { Uuid } from '@types';
+// * ---------------------------------------------------------------------- * //
+
+export interface ManagerUpdateData {
+  id: Uuid;
+  name?: string;
+  lastName?: string;
+  password?: string;
+  updatedAt?: Date;
+}
