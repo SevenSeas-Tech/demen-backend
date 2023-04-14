24b1c3d46ba03581ea443abdb857ed4c5a143691
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-14 17:11:52 -0300

    feat(management): ao criar um manager, cria-se tambem um email relacionado a um tipo

--- src/modules/management/infra/database/prisma/repositories/managers.ts
+++ src/modules/management/infra/database/prisma/repositories/managers.ts
@@ -1,5 +1,3 @@
-import { PrismaClient } from '@prisma/client';
-
 import type { ManagerCreationData } from '@management:dto/manager/create';
 import type { FullName } from '@management:dto/manager/full-name';
 import type { ManagerQueryOptions } from '@management:dto/manager/query';
@@ -8,12 +6,18 @@ import type { Manager } from '@management:models/manager';
 import type { ManagersRepository } from '@management:repositories/managers';
 import type { Uuid } from '@types';
 import type {
+  EmailNestedInput,
+  EmailTypeNestedInput
+} from '@management:database-types/prisma/email';
+import type {
   ManagerWhereInput,
   ManagerUpdateInput,
   ManagerWhereUniqueInput,
   ManagerInclude,
   ManagerCreateInput
 } from '@management:database-types/prisma/manager';
+
+import { PrismaDatabase } from '@shared/infra/database/prisma/prisma-database';
 
 // * ---------------------------------------------------------------------- * //
 
@@ -23,7 +27,8 @@ class PrismaManagersRepository implements ManagersRepository {
   // *** --- interns ---------------------------------------------------- *** //
 
   constructor() {
-    this.repository = new PrismaClient().manager;
+    const prisma = PrismaDatabase.getInstance();
+    this.repository = prisma.client.manager;
   }
 
   // ------------------------------------------------------------------------ //
@@ -47,7 +52,23 @@ class PrismaManagersRepository implements ManagersRepository {
   // *** --- methods ---------------------------------------------------- *** //
 
   async create(data: ManagerCreationData): Promise<Manager> {
-    const prismaData: ManagerCreateInput = data;
+    const { lastName, name, password, isActive, email, emailType:type } = data;
+
+    const emailType: EmailTypeNestedInput = {
+      connect: { type }
+    };
+
+    const emails: EmailNestedInput = {
+      create: { email, emailType }
+    };
+
+    const prismaData: ManagerCreateInput = {
+      name,
+      lastName,
+      password,
+      isActive,
+      emails
+    };
 
     const manager = await this.repository.create({ data: prismaData });
 
