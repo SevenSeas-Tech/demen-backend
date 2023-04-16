0cae5075c1e271a1f3cdba94ba90520d1035f4b0
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-16 17:58:53 -0300

    feat(management): adicionado o método update a interface de repositorio de emails

--- src/modules/management/@types/repositories/emails.d.ts
+++ src/modules/management/@types/repositories/emails.d.ts
@@ -1,7 +1,13 @@
+import type { EmailCreationData } from '@management:dto/email/create';
 import type { Email } from '@management:models/email';
+import type { Uuid } from '@types';
 
 // * ---------------------------------------------------------------------- * //
 
 export interface EmailsRepository {
-  findOne(email: string): Promise<Email | undefined>;
+  create(data: EmailCreationData): Promise<Email>;
+  delete(email: string): Promise<void>;
+  update(email: string, updatedEmail: string): Promise<Email>;
+  findByEmail(email: string): Promise<Email | undefined>;
+  findByUser(userId: Uuid): Promise<Email[]>;
 }
