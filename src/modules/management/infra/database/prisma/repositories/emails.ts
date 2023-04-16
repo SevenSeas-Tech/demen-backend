73b0cdbc8ed008977ef2f840a9fe4c8cefbb79c1
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-16 18:04:20 -0300

    feat(management): criado o repositorio de emails

---
+++ src/modules/management/infra/database/prisma/repositories/emails.ts
@@ -0,0 +1,75 @@
+import type { EmailsRepository } from '@management:repositories/emails';
+import type { Email } from '@management:models/email';
+import type { EmailCreationData } from '@management:dto/email/create';
+import type {
+  EmailDelegate,
+  EmailUpdateInput,
+  EmailWhereInput,
+  EmailWhereUniqueInput
+} from '@management:database-types/prisma/email';
+
+import { PrismaDatabase } from '@shared/infra/database/prisma/prisma-database';
+
+// * ---------------------------------------------------------------------- * //
+
+class PrismaEmailsRepository implements EmailsRepository {
+  private repository: EmailDelegate;
+
+  constructor() {
+    this.repository = PrismaDatabase.getInstance().client.email;
+  }
+
+  // *** --- methods ---------------------------------------------------- *** //
+
+  async create(data: EmailCreationData): Promise<Email> {
+    const email = await this.repository.create({ data });
+
+    return email;
+  }
+
+  // ------------------------------------------------------------------------ //
+
+  async delete(email: string): Promise<void> {
+    const where: EmailWhereUniqueInput = { email };
+
+    await this.repository.delete({ where });
+
+    return;
+  }
+
+  // ------------------------------------------------------------------------ //
+
+  async update(address: string, updatedAddress: string): Promise<Email> {
+    const where: EmailWhereUniqueInput = { email: address };
+
+    const data: EmailUpdateInput = { email: updatedAddress };
+
+    const email = await this.repository.update({ where, data });
+
+    return email;
+  }
+
+  // ------------------------------------------------------------------------ //
+
+  async findByEmail(address: string): Promise<Email | undefined> {
+    const where: EmailWhereUniqueInput = { email: address };
+
+    const email = await this.repository.findUnique({ where });
+
+    return email as Email;
+  }
+
+  // ------------------------------------------------------------------------ //
+
+  async findByUser(userId: string): Promise<Email[]> {
+    const where: EmailWhereInput = { userId };
+
+    const emails = await this.repository.findMany({ where });
+
+    return emails;
+  }
+}
+
+// * ---------------------------------------------------------------------- * //
+
+export { PrismaEmailsRepository };
