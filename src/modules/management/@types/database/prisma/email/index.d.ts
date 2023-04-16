1074bed15b1e49c99da8943381df746af587a612
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-16 17:55:47 -0300

    feat(management): criado o arquivo de tipos do prisma para o cliente de email

--- src/modules/management/@types/database/prisma/email/index.d.ts
+++ src/modules/management/@types/database/prisma/email/index.d.ts
@@ -2,6 +2,14 @@ import type { Prisma } from '@prisma/client';
 
 // * ---------------------------------------------------------------------- * //
 
+export type EmailDelegate = Prisma.EmailDelegate<undefined>;
+
 export type EmailNestedInput = Prisma.EmailCreateNestedManyWithoutUserInput;
 
 export type EmailTypeNestedInput = Prisma.EmailTypeCreateNestedOneWithoutEmailsInput;
+
+export type EmailUpdateInput = Prisma.EmailUpdateInput;
+
+export type EmailWhereInput = Prisma.EmailWhereInput;
+
+export type EmailWhereUniqueInput = Prisma.EmailWhereUniqueInput;
