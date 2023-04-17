0de731940b5259bae9dcd425e8a87cf705787183
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-17 13:45:51 -0300

    fix(management): corrigido o dto para update de emailt type na interface de repositório

--- src/modules/management/@types/repositories/email-types.d.ts
+++ src/modules/management/@types/repositories/email-types.d.ts
@@ -1,11 +1,13 @@
 import type { EmailType } from '@management:models/email-type';
+import type { EmailTypeCreationData } from '@management:dto/email-type/create';
+import type { EmailTypeUpdateData } from '@management:dto/email-type/update';
 
 // * ---------------------------------------------------------------------- * //
 
 interface EmailTypesRepository {
   create(data: EmailTypeCreationData): Promise<EmailType>;
   findByType(type: string): Promise<EmailType | undefined>;
-  update(type: string): Promise<EmailType>;
+  update(data: EmailTypeUpdateData): Promise<EmailType>;
   delete(type: string): Promise<void>;
 }
 
