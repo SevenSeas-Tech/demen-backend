bcea2a330023d5a3f1cbb1b4664c35caf24b9285
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-17 13:48:01 -0300

    feat(management/prisma): criado o arquivo de tipos do prisma para o repositorio de email types

---
+++ src/modules/management/@types/database/prisma/email/type.d.ts
@@ -0,0 +1,11 @@
+import type { Prisma } from '@prisma/client';
+
+// * ---------------------------------------------------------------------- * //
+
+export type EmailTypeDelegate = Prisma.EmailTypeDelegate<undefined>;
+
+export type EmailTypeCreateInput = Prisma.EmailTypeCreateInput;
+
+export type EmailTypeWhereUniqueInput = Prisma.EmailTypeWhereUniqueInput;
+
+export type EmailTypeUpdateInput = Prisma.EmailTypeUpdateInput;
