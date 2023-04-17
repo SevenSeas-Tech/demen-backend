3450d619b285222599ec17d700cbdf378b9e3a37
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-17 12:23:02 -0300

    feat(management): implementado o retorno do manager junto ao email no repositorio de emails

--- src/modules/management/@types/database/prisma/email/index.d.ts
+++ src/modules/management/@types/database/prisma/email/index.d.ts
@@ -4,6 +4,8 @@ import type { Prisma } from '@prisma/client';
 
 export type EmailDelegate = Prisma.EmailDelegate<undefined>;
 
+export type EmailInclude = Prisma.EmailInclude
+
 export type EmailNestedInput = Prisma.EmailCreateNestedManyWithoutUserInput;
 
 export type EmailTypeNestedInput = Prisma.EmailTypeCreateNestedOneWithoutEmailsInput;
--- src/modules/management/infra/database/prisma/repositories/emails.ts
+++ src/modules/management/infra/database/prisma/repositories/emails.ts
@@ -3,6 +3,7 @@ import type { Email } from '@management:models/email';
 import type { EmailCreationData } from '@management:dto/email/create';
 import type {
   EmailDelegate,
+  EmailInclude,
   EmailUpdateInput,
   EmailWhereInput,
   EmailWhereUniqueInput
@@ -13,11 +14,8 @@ import { PrismaDatabase } from '@shared/infra/database/prisma/prisma-database';
 // * ---------------------------------------------------------------------- * //
 
 class PrismaEmailsRepository implements EmailsRepository {
-  private repository: EmailDelegate;
-
-  constructor() {
-    this.repository = PrismaDatabase.getInstance().client.email;
-  }
+  private repository: EmailDelegate = PrismaDatabase.getInstance().client.email;
+  private include: EmailInclude = { user: true };
 
   // *** --- methods ---------------------------------------------------- *** //
 
@@ -66,7 +64,8 @@ class PrismaEmailsRepository implements EmailsRepository {
   async findByEmail(address: string): Promise<Email | undefined> {
     const where: EmailWhereUniqueInput = { email: address };
 
-    const email = await this.repository.findUnique({ where });
+    const email = await this.repository
+      .findUnique({ where, include: this.include });
 
     return email as Email;
   }
@@ -76,7 +75,8 @@ class PrismaEmailsRepository implements EmailsRepository {
   async findByUser(userId: string): Promise<Email[]> {
     const where: EmailWhereInput = { userId };
 
-    const emails = await this.repository.findMany({ where });
+    const emails = await this.repository
+      .findMany({ where, include: this.include });
 
     return emails;
   }
