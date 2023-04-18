ed2003e4783ca91070bf9914e98d795762368601
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-04-18 16:53:33 -0300

    feat(shared): adicionada a rota para o modulo management

--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -1,17 +1,20 @@
+import { lessonsRouter } from '@content:infra/http/routes/index.routes';
+import { managementRouter } from '@management:infra/http/routes/index.routes';
 import { Router } from 'express';
 
-import { accountsRouter } from '@accounts:routes/index.routes';
-import { lessonsRouter } from '@lessons:routes/index.routes';
-
-// ---------------------------------------------------------------------------------------------- //
+// * ---------------------------------------------------------------------- * //
 
 const routes = Router();
 
-// ---------------------------------------------------------------------------------------------- //
+// -------------------------------------------------------------------------- //
 
-routes.use('/accounts', accountsRouter);
-routes.use('/lessons', lessonsRouter);
+routes.use('/content', lessonsRouter);
+routes.use('/education');
+routes.use('/management', managementRouter);
+routes.use('/students');
 
-// ---------------------------------------------------------------------------------------------- //
+// * ---------------------------------------------------------------------- * //
 
 export { routes };
+
+// todo: implement all routes. only management is for now.
