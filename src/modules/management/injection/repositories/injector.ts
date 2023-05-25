33cb3e8a156abe77a189748984710b5529c711ca
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-05-25 12:40:22 -0300

    feat(management): added test repositories into injection container

--- src/modules/management/injection/repositories/injector.ts
+++ src/modules/management/injection/repositories/injector.ts
@@ -1,16 +1,27 @@
+
 import { PrismaEmailTypesRepository } from '@management:database/prisma/repositories/email-types';
 import { PrismaEmailsRepository } from '@management:database/prisma/repositories/emails';
 import { PrismaManagersRepository } from '@management:database/prisma/repositories/managers';
-import { ManagersTestRepository } from '@management:database/test/repositories/managers';
+import {
+  EmailTypesTestRepository
+} from '@management:database/test/repositories/email-types-test-repository';
+import {
+  EmailsTestRepository
+} from '@management:database/test/repositories/emails-test-repository';
+import {
+  ManagersTestRepository
+} from '@management:database/test/repositories/managers-test-repository';
 import {
   EmailTypesRepositorySymbol,
   EmailsRepositorySymbol,
   ManagersRepositorySymbol,
+  TestEmailTypesRepositorySymbol,
+  TestEmailsRepositorySymbol,
   TestManagersRepositorySymbol
 } from '@management:injection/repositories/symbols';
 
-import type { EmailTypesRepository } from '@management:repositories/email-types';
-import type { EmailsRepositoryInterface } from '@management:repositories/emails';
+import type { EmailTypesRepositoryInterface } from '@management:repositories/email-types';
+import type { EmailsRepositoryInterface } from '@management:repositories/emails-repository';
 import type { ManagersRepositoryInterface } from '@management:repositories/managers';
 
 // * ---------------------------------------------------------------------- * //
@@ -25,8 +36,14 @@ class ManagementRepositoriesContainer {
   readonly [EmailsRepositorySymbol]: EmailsRepositoryInterface =
     new PrismaEmailsRepository();
 
-  readonly [EmailTypesRepositorySymbol]: EmailTypesRepository =
+  readonly [TestEmailsRepositorySymbol]: EmailsRepositoryInterface =
+    new EmailsTestRepository();
+
+  readonly [EmailTypesRepositorySymbol]: EmailTypesRepositoryInterface =
     new PrismaEmailTypesRepository();
+
+  readonly [TestEmailTypesRepositorySymbol]: EmailTypesRepositoryInterface =
+    new EmailTypesTestRepository();
 }
 
 // * ---------------------------------------------------------------------- * //
