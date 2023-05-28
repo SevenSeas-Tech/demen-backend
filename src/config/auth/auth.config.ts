a7e9007e2f724694582fd1206d8ce0ea7a10d285
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-05-28 19:13:47 -0300

    refactor(config): adicionado as const na configuração de auth

--- src/config/auth/auth.config.ts
+++ src/config/auth/auth.config.ts
@@ -6,4 +6,4 @@ export const authConfig = {
   secretJwt: process.env.JWT_SECRET as string,
   secretRefresh: process.env.JWT_REFRESH_SECRET as string,
   secretPassword: process.env.JWT_PASSWORD_SECRET as string
-};
+} as const;
